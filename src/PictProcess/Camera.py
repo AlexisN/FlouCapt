a4fde6f2ce1f2da7ab38357d26c88d7e92a18c41
Kevin-Bannier <bannier.kevin@orange.fr>
2014-01-06 18:18:25 +0100

    addition of GetPicture method to retrieve the image of a webcam

--- src/PictProcess/Camera.py
+++ src/PictProcess/Camera.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python2
 
+import cv2
 
 class Camera:
+
+
+    @staticmethod
+    def getPicture():
+        #vc = cv2.VideoCapture("http://192.168.0.9/videostream.asf?user=guest&pwd=guest")
+        vc = cv2.VideoCapture(0)
+
+        if vc.isOpened(): # try to get the first frame
+            rval, img = vc.read()
+        else:
+            rval = False
+
+        return rval, img;
