583f711efd93250a6b9ac65d4845e73aa32b8bfa
Kevin Bannier <bannier.kevin@orange.fr>
2014-03-31 17:52:04 +0200

    Update tests

--- PictProcess/floucapt/tests/mem_disk_Usage.py
+++ PictProcess/floucapt/tests/mem_disk_Usage.py
@@ -32,24 +32,25 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, signal_handler)
 
     filee = open( 'mem_disk_Usage.csv' ,'w+')
-    filee.write('pos,memory,disk\n')
+    #filee.write('pos,memory,disk\n')
+    filee.write('hour\t\tmemory\n')
     filee.close()
 
     i = 0
     global quit
     quit = False
 
-    time.sleep(3)
+    time.sleep(5)
 
     while not quit:
-        i +=1
         memory = os.popen('pmap -x ' + str(pid) + '|grep total')
         memory = memory.read()
         memory = memory.split(' ')
         memory = memory [ len(memory) -1 ]
         memory = memory.strip('\n')
 
-        disk = os.popen('du /var/floucapt')
+        #disk = os.popen('du /var/floucapt')
+        disk = os.popen('du ../out/pictures')
         disk = disk.read()
 
         disk = disk.split('\n')
@@ -61,13 +62,15 @@ if __name__ == '__main__':
 
 
         filee = open( 'mem_disk_Usage.csv' ,'a')
-        filee.write(str(i) +',' + memory + ','+ disk+'\n')
+        #filee.write(str(i) +',' + memory + ','+ disk+'\n')
+        filee.write(str(i) + " hours\t\tRAM : " + memory + "ko\n")
         filee.close()
 
 
-        print "Salut Michel !"
+        print "Write mem_disk_Usage.csv"
         if not quit:
-            time.sleep(10)
+            time.sleep(3600)
+        i +=1
 
 
 
